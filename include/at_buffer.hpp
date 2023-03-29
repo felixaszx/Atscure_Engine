35c99bf174a6b0d3545354c9796fde3ddff81e0d
Felix Xing <101950667+felixxing@users.noreply.github.com>
2023-03-29 08:43:47 -0400

    update buffer clas

--- include/at_buffer.hpp
+++ include/at_buffer.hpp
@@ -2,11 +2,27 @@
 #define AT_BUFFER_HPP
 
 #include "at_device.hpp"
+#include "at_cmd.hpp"
 
 namespace ats
 {
-    using Buffer = MultiType<VkBuffer, VkDeviceMemory, VmaAllocation>;
-    
-};
+    class Buffer : iMultiType(VkBuffer, VkDeviceMemory, VmaAllocation)
+    {
+      private:
+        bool mapped = false;
+        VmaMemoryUsage memory_ = VMA_MEMORY_USAGE_AUTO;
+        VmaAllocationCreateFlagBits allocation_flags_ = {};
+
+      public:
+        Buffer(VmaMemoryUsage memory_usage, VmaAllocationCreateFlagBits allocation_flags);
+        void create(Device& device, VkBufferUsageFlags usage, VkSharingMode sharing, VkDeviceSize size);
+        void* create_mapped(Device& device, VkBufferUsageFlags usage, VkSharingMode sharing, VkDeviceSize size);
+        void destroy(Device& device);
+    };
+
+    void buffer_cpy(Device& device, VkBuffer src, VkBuffer dst, VkBufferCopy region);
+    void buffer_cpy(Device& device, VkCommandPool pool, VkBuffer src, VkBuffer dst, VkBufferCopy region);
+
+}; // namespace ats
 
 #endif // AT_BUFFER_HPP
--- src/at_buffer.cpp
+++ src/at_buffer.cpp
@@ -2,4 +2,56 @@
 
 namespace ats
 {
-};
+    void Buffer::create(Device& device, VkBufferUsageFlags usage, VkSharingMode sharing, VkDeviceSize size)
+    {
+        VkBufferCreateInfo buffer_info{};
+        buffer_info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+        buffer_info.usage = usage;
+        buffer_info.sharingMode = sharing;
+        buffer_info.size = size;
+        VmaAllocationCreateInfo alloc_info{};
+        alloc_info.usage = memory_;
+        alloc_info.flags = allocation_flags_;
+
+        VmaAllocationInfo alloc_result{};
+        vmaCreateBuffer(device, &buffer_info, &alloc_info, this->ptr(), this->ptr(), &alloc_result);
+        this->set(alloc_result.deviceMemory);
+    }
+
+    void* Buffer::create_mapped(Device& device, VkBufferUsageFlags usage, VkSharingMode sharing, VkDeviceSize size)
+    {
+        create(device, usage, sharing, size);
+        void* mapping = nullptr;
+        vmaMapMemory(device, *this, &mapping);
+        mapped = true;
+
+        return mapping;
+    }
+
+    void Buffer::destroy(Device& device)
+    {
+        if (mapped)
+        {
+            vmaUnmapMemory(device, *this);
+        }
+
+        vmaDestroyBuffer(device, *this, *this);
+    }
+
+    void buffer_cpy(Device& device, VkBuffer src, VkBuffer dst, VkBufferCopy region)
+    {
+        SingleTimeCmd cmd;
+        cmd.begin(device);
+        vkCmdCopyBuffer(cmd, src, dst, 1, &region);
+        cmd.end(device);
+    }
+
+    void buffer_cpy(Device& device, VkCommandPool pool, VkBuffer src, VkBuffer dst, VkBufferCopy region)
+    {
+        SingleTimeCmd cmd;
+        cmd.begin(device, pool);
+        vkCmdCopyBuffer(cmd, src, dst, 1, &region);
+        cmd.end(device, pool);
+    }
+
+}; // namespace ats
