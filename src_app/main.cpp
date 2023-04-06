f798b629f637189836ba047553b806ca806b6750
Felix Xing <101950667+felixxing@users.noreply.github.com>
2023-04-06 14:54:56 -0400

    render pass created

--- src_app/main.cpp
+++ src_app/main.cpp
@@ -77,5 +77,40 @@ int main(int argc, char** argv)
     reference[2].push_back({4, vk::ImageLayout::eShaderReadOnlyOptimal});
     reference[2].push_back({6, vk::ImageLayout::eColorAttachmentOptimal});
 
+    std::vector<vk::SubpassDescription> subpass(3);
+    subpass[0].colorAttachmentCount = 4;
+    subpass[0].pColorAttachments = reference[0].data();
+    subpass[0].pDepthStencilAttachment = reference[0].data() + 4;
+
+    subpass[1].inputAttachmentCount = 4;
+    subpass[1].pInputAttachments = reference[1].data();
+    subpass[1].colorAttachmentCount = 1;
+    subpass[1].pColorAttachments = reference[1].data() + 4;
+
+    subpass[2].inputAttachmentCount = 1;
+    subpass[2].pInputAttachments = reference[2].data();
+    subpass[2].colorAttachmentCount = 1;
+    subpass[2].pColorAttachments = reference[2].data() + 1;
+    render_detail.subpass_ = subpass;
+
+    std::vector<vk::SubpassDependency> dependency(2);
+    dependency[0].srcSubpass = 0;
+    dependency[0].dstSubpass = 1;
+    dependency[0].srcStageMask = vk::PipelineStageFlagBits::eColorAttachmentOutput | //
+                                 vk::PipelineStageFlagBits::eLateFragmentTests;
+    dependency[0].dstStageMask = vk::PipelineStageFlagBits::eFragmentShader;
+    dependency[0].srcAccessMask = vk::AccessFlagBits::eColorAttachmentWrite | //
+                                  vk::AccessFlagBits::eDepthStencilAttachmentWrite;
+    dependency[0].dstAccessMask = vk::AccessFlagBits::eInputAttachmentRead;
+
+    dependency[1].srcSubpass = 1;
+    dependency[1].dstSubpass = 2;
+    dependency[1].srcStageMask = vk::PipelineStageFlagBits::eColorAttachmentOutput;
+    dependency[1].dstStageMask = vk::PipelineStageFlagBits::eFragmentShader;
+    dependency[1].srcAccessMask = vk::AccessFlagBits::eColorAttachmentWrite;
+    dependency[1].dstAccessMask = vk::AccessFlagBits::eInputAttachmentRead;
+    render_detail.dependency_ = dependency;
+    as::RenderPass render_pass = rnew as::RenderPass(render_detail);
+
     return EXIT_SUCCESS;
 }
