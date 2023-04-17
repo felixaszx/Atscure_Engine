449e212ac41549e1820cb1c16ee977de373e8749
Felix Xing <101950667+felixxing@users.noreply.github.com>
2023-04-16 22:25:34 -0400

    shrink codes

--- src/platform/script.cpp
+++ src/platform/script.cpp
@@ -2,13 +2,8 @@
 
 as::Script::Script(as::DynamicLoader& loader)
 {
-    funcs[INIT] = LoadFunc(loader, FUNC_NAMES[INIT]);
-    funcs[FINISH] = LoadFunc(loader, FUNC_NAMES[FINISH]);
-    funcs[WRITE] = LoadFunc(loader, FUNC_NAMES[WRITE]);
-    funcs[READ] = LoadFunc(loader, FUNC_NAMES[READ]);
-
-    funcs[START] = LoadFunc(loader, FUNC_NAMES[START]);
-    funcs[END] = LoadFunc(loader, FUNC_NAMES[END]);
-    funcs[UPDATE] = LoadFunc(loader, FUNC_NAMES[UPDATE]);
-    funcs[FIXED] = LoadFunc(loader, FUNC_NAMES[FIXED]);
+    for (int i = 0; i < FUNC_COUNTS; i++)
+    {
+        funcs[i] = LoadFunc(loader, FUNC_NAMES[i]);
+    }
 }
