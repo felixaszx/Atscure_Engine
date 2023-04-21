2f93537461fdcb37ffffab39c22ab0ac3779116b
Felix Xing <101950667+felixxing@users.noreply.github.com>
2023-04-21 09:49:15 -0400

    working on scene now

---
+++ include/as/scene.hpp
@@ -0,0 +1,51 @@
+#ifndef SCENE_HPP
+#define SCENE_HPP
+
+#include <entt/entt.hpp>
+#include "mesh.hpp"
+
+namespace as
+{
+    class Entity
+    {
+      private:
+        entt::entity e_{};
+        entt::registry* reg_{};
+
+      public:
+        template <typename Component>
+        Component* get()
+        {
+            if (reg_->valid(e_))
+            {
+                return reg_->try_get<Component>(e_);
+            }
+            return nullptr;
+        }
+    };
+
+    struct Scene
+    {
+        entt::registry reg_{};
+    };
+
+    struct CameraComp
+    {
+    };
+
+    struct TransformComp
+    {
+    };
+
+    struct ScriptComp
+    {
+    };
+
+    struct MeshComp
+    {
+        Mesh* mesh_ = nullptr;
+    };
+
+}; // namespace as
+
+#endif // SCENE_HPP
