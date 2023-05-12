3dfbe0f798e81f5d482ddf7bdc354a8c5aecd6cf
Felix Xing <101950667+felixxing@users.noreply.github.com>
2023-05-11 22:34:42 -0400

    ready

--- src_app/main.cpp
+++ src_app/main.cpp
@@ -2,10 +2,10 @@
 #include <string>
 #include "vk_api/wrapper.hpp"
 #include "as/wrapper.hpp"
-#include "module.hpp"
-       
+#include "module.hpp"   
 
 int main(int argc, char** argv)
 {
+    as::ModuleManager modules;
     return EXIT_SUCCESS;
 }
