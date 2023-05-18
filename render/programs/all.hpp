970f0de80a100404bfc1f8136f2cef93969a2a58
Felix Xing <101950667+felixxing@users.noreply.github.com>
2023-05-17 21:49:49 -0400

    ?

--- render/programs/all.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef ALL_HPP
-#define ALL_HPP
-
-#include "deffered_zero.hpp"
-
-#endif // ALL_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,7 @@
 #include <iostream>
-#include "render/render.hpp"
 
 int main(int argc, char** argv)
 {
-    as::RenderContext render_context(1920, 1080);
-
-    as::MeshDataGroup group(10);
-    group.add_buffers({{}, {}, {}, {}}, {1, 2, 3, 4}, render_context.utils_pool());
-
-    as::DefferedZero d(render_context.vk_swapchain_);
-    d.update_ubo({}, {});
-
-    while (render_context.running())
-    {
-        render_context.update_window();
-    }
 
     return EXIT_SUCCESS;
 }
